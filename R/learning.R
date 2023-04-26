7039b055f446c54dfe4c8a7a154bcd0ce9812552
Kaja Madsen <kajamadsen@health.sdu.dk>
2023-04-26 14:00:49 +0200

    Adding excercise 7.15 to learningR

--- R/learning.R
+++ R/learning.R
@@ -103,4 +103,34 @@ nhanes_modified <- nhanes_small %>% # Specifying dataset
         young_child = if_else(age < 6, "Yes", "No")
     )
 
-    nhanes_modified
+nhanes_modified
+
+# Creating summary statistics
+nhanes_small %>%
+    summarise(max_bmi = max(bmi, na.rm = TRUE),
+              min_bmi = min(bmi, na.rm=TRUE))
+
+nhanes_small %>%
+    filter(!is.na(diabetes)) %>%
+    group_by(diabetes) %>%
+    summarise(mean_age = mean(age, na.rm =TRUE),
+              mean_bmi = mean(bmi, na.rm=TRUE)) %>%
+    ungroup()
+
+# Saving data
+readr::write_csv(nhanes_small, here::here("data/nhanes_small.csv"))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
